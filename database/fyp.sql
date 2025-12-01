f9ef237277a4ede422d3b44644f063c6a4e8d683
DUISINCONG <167316028+DUISINCONG@users.noreply.github.com>
2025-12-01 17:21:49 +0800

    Update fyp.sql

--- database/fyp.sql
+++ database/fyp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2025 at 10:45 AM
+-- Generation Time: Dec 01, 2025 at 10:21 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -32,17 +32,18 @@ CREATE TABLE `admins` (
   `admin_name` varchar(255) NOT NULL,
   `admin_password` varchar(255) NOT NULL,
   `role` enum('superadmin','admin') NOT NULL,
-  `admin_status` varchar(255) DEFAULT NULL
+  `admin_status` varchar(255) DEFAULT NULL,
+  `admin_email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admins`
 --
 
-INSERT INTO `admins` (`admin_id`, `admin_name`, `admin_password`, `role`, `admin_status`) VALUES
-(1, 'DuiSinCong', 'Abc12345?', 'superadmin', 'active'),
-(2, 'Jerry', 'Jerry4141&', 'admin', 'active'),
-(3, 'Keishav', 'Keishav5856!', 'admin', 'active');
+INSERT INTO `admins` (`admin_id`, `admin_name`, `admin_password`, `role`, `admin_status`, `admin_email`) VALUES
+(1, 'DuiSinCong', 'f2652931', 'superadmin', 'active', 'duisincong1121@gmail.com'),
+(2, 'Jerry', 'Jerry4141&', 'admin', 'active', 'jerry@gmail.com'),
+(3, 'Keishav', 'Keishav5856!', 'admin', 'active', 'keishav@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -55,22 +56,23 @@ CREATE TABLE `beverages` (
   `product_name` varchar(100) NOT NULL,
   `product_price` decimal(5,2) NOT NULL,
   `product_status` varchar(20) NOT NULL,
-  `product_description` text DEFAULT NULL
+  `product_description` text DEFAULT NULL,
+  `product_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `beverages`
 --
 
-INSERT INTO `beverages` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`) VALUES
-('D01', 'Orange Juice', 6.90, 'Available', NULL),
-('D02', 'Watermelon Juice', 6.90, 'Available', NULL),
-('D03', 'Orange Yogurt', 7.90, 'Available', NULL),
-('D04', 'Watermelon Yogurt', 7.90, 'Available', NULL),
-('D05', 'Peach Tea', 5.90, 'Available', ''),
-('D06', 'Lemonade', 5.90, 'Available', ''),
-('D07', 'Honey Lemon', 6.90, 'Available', ''),
-('D08', 'Skyjuice', 0.50, 'Available', '');
+INSERT INTO `beverages` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`, `product_image`) VALUES
+('D01', 'Orange Juice', 6.90, 'Available', '', 'uploads/D01.jpg'),
+('D02', 'Watermelon Juice', 6.90, 'Available', '', 'uploads/D02.jpg'),
+('D03', 'Orange Yogurt', 7.90, 'Available', '', 'uploads/D03.jpg'),
+('D04', 'Watermelon Yogurt', 7.90, 'Available', '', 'uploads/D04.jpg'),
+('D05', 'Peach Tea', 5.90, 'Available', '', 'uploads/D05.jpg'),
+('D06', 'Lemonade', 5.90, 'Available', '', 'uploads/D06.jpg'),
+('D07', 'Honey Lemon', 6.90, 'Available', '', 'uploads/D07.jpg'),
+('D08', 'Skyjuice', 0.50, 'Available', '', 'uploads/D08.jpg');
 
 -- --------------------------------------------------------
 
@@ -83,19 +85,20 @@ CREATE TABLE `coffee` (
   `product_name` varchar(100) NOT NULL,
   `product_price` decimal(5,2) NOT NULL,
   `product_status` varchar(20) NOT NULL,
-  `product_description` text DEFAULT NULL
+  `product_description` text DEFAULT NULL,
+  `product_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `coffee`
 --
 
-INSERT INTO `coffee` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`) VALUES
-('C01', 'Americano', 6.90, 'Available', NULL),
-('C02', 'Caffe Latte', 7.90, 'Available', NULL),
-('C03', 'Matcha Latte', 8.90, 'Available', NULL),
-('C04', 'Mocha Latte', 9.90, 'Available', NULL),
-('C05', 'Caramel Latte', 9.90, 'Available', NULL);
+INSERT INTO `coffee` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`, `product_image`) VALUES
+('C01', 'Americano', 6.90, 'Available', '', 'uploads/C01.jpg'),
+('C02', 'Caffe Latte', 7.90, 'Available', '', 'uploads/C02.jpg'),
+('C03', 'Matcha Latte', 8.90, 'Available', '', 'uploads/C03.jpg'),
+('C04', 'Mocha Latte', 9.90, 'Available', '', 'uploads/C04.jpg'),
+('C05', 'Caramel Latte', 9.90, 'Available', '', 'uploads/C05.jpg');
 
 -- --------------------------------------------------------
 
@@ -131,34 +134,35 @@ CREATE TABLE `main_food` (
   `product_name` varchar(100) NOT NULL,
   `product_price` decimal(5,2) NOT NULL,
   `product_status` varchar(20) NOT NULL,
-  `product_description` text DEFAULT NULL
+  `product_description` text DEFAULT NULL,
+  `product_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `main_food`
 --
 
-INSERT INTO `main_food` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`) VALUES
-('A01', 'Chicken Chop & Aglio Olio', 15.90, 'Available', ''),
-('A02', 'Chicken Chop & Bolognese Spaghetti', 15.90, 'Available', NULL),
-('A03', 'Chicken Chop & Creamy Carbonara', 15.90, 'Available', NULL),
-('A04', 'Salmon Steak & Aglio Olio', 25.90, 'Available', NULL),
-('A05', 'Salmon Steak & Bolognese Spaghetti', 25.90, 'Available', NULL),
-('A06', 'Salmon Steak & Creamy Carbonara', 25.90, 'Available', NULL),
-('A07', 'Grilled Chicken', 19.90, 'Available', NULL),
-('A08', 'Grilled Lamb Shoulder', 19.90, 'Available', NULL),
-('A09', 'Smoked Duck', 19.90, 'Available', NULL),
-('A10', 'Fried Chicken Chop', 20.90, 'Available', NULL),
-('A11', 'Hawaiian Style Chicken Chop', 23.90, 'Available', NULL),
-('A12', 'Fish & Chips', 22.90, 'Available', NULL),
-('A13', 'Salmon Steak', 30.90, 'Available', NULL),
-('A14', 'Fried Chicken Chop & Rice', 14.90, 'Available', NULL),
-('A15', 'Fish Chop & Rice', 14.90, 'Available', NULL),
-('A16', 'Grilled Chicken & Rice', 16.90, 'Available', NULL),
-('A17', 'Curry Chicken Chop Rice', 15.90, 'Available', NULL),
-('A18', 'Curry Fish Chop Rice', 15.90, 'Available', NULL),
-('A19', 'Japanese Chicken Donburi', 15.90, 'Available', NULL),
-('A20', 'Smoked Duck Donburi', 15.90, 'Available', NULL);
+INSERT INTO `main_food` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`, `product_image`) VALUES
+('A01', 'Chicken Chop & Aglio Olio', 15.90, 'Available', '', 'uploads/A01.jpg'),
+('A02', 'Chicken Chop & Bolognese Spaghetti', 15.90, 'Available', '', 'uploads/A02.jpg'),
+('A03', 'Chicken Chop & Creamy Carbonara', 15.90, 'Available', '', 'uploads/A03.jpg'),
+('A04', 'Salmon Steak & Aglio Olio', 25.90, 'Available', '', 'uploads/A04.jpg'),
+('A05', 'Salmon Steak & Bolognese Spaghetti', 25.90, 'Available', '', 'uploads/A05.jpg'),
+('A06', 'Salmon Steak & Creamy Carbonara', 25.90, 'Available', '', 'uploads/A06.jpg'),
+('A07', 'Grilled Chicken', 19.90, 'Available', '', 'uploads/A07.jpg'),
+('A08', 'Grilled Lamb Shoulder', 19.90, 'Available', '', 'uploads/A08.jpg'),
+('A09', 'Smoked Duck', 19.90, 'Available', '', 'uploads/A09.jpg'),
+('A10', 'Fried Chicken Chop', 20.90, 'Available', '', 'uploads/A10.jpg'),
+('A11', 'Hawaiian Style Chicken Chop', 23.90, 'Available', '', 'uploads/A11.jpg'),
+('A12', 'Fish & Chips', 22.90, 'Available', '', 'uploads/A12.jpg'),
+('A13', 'Salmon Steak', 30.90, 'Available', '', 'uploads/A13.jpg'),
+('A14', 'Fried Chicken Chop & Rice', 14.90, 'Available', '', 'uploads/A14.jpg'),
+('A15', 'Fish Chop & Rice', 14.90, 'Available', '', 'uploads/A15.jpg'),
+('A16', 'Grilled Chicken & Rice', 16.90, 'Available', '', 'uploads/A16.jpg'),
+('A17', 'Curry Chicken Chop Rice', 15.90, 'Available', '', 'uploads/A17.jpg'),
+('A18', 'Curry Fish Chop Rice', 15.90, 'Available', '', 'uploads/A18.jpg'),
+('A19', 'Japanese Chicken Donburi', 15.90, 'Available', '', 'uploads/A19.jpg'),
+('A20', 'Smoked Duck Donburi', 15.90, 'Available', '', 'uploads/A20.jpg');
 
 -- --------------------------------------------------------
 
@@ -185,21 +189,22 @@ CREATE TABLE `pizza` (
   `product_name` varchar(100) NOT NULL,
   `product_price` decimal(5,2) NOT NULL,
   `product_status` varchar(20) NOT NULL,
-  `product_description` text DEFAULT NULL
+  `product_description` text DEFAULT NULL,
+  `product_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pizza`
 --
 
-INSERT INTO `pizza` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`) VALUES
-('B01', 'Hawaiian Chicken Pizza', 19.90, 'Available', NULL),
-('B02', 'Mushroom Pizza', 19.90, 'Available', NULL),
-('B03', 'Seafood & Cheese Pizza', 19.90, 'Available', NULL),
-('B04', 'Chicken & Mushroom Pizza', 19.90, 'Available', NULL),
-('B05', 'Triple Cheese Pizza', 19.90, 'Available', NULL),
-('B06', 'Beef & Chicken Pizza', 19.90, 'Available', NULL),
-('B07', 'BBQ Chicken Pizza', 19.90, 'Available', NULL);
+INSERT INTO `pizza` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`, `product_image`) VALUES
+('B01', 'Hawaiian Chicken Pizza', 19.90, 'Available', '', 'uploads/B01.jpg'),
+('B02', 'Mushroom Pizza', 19.90, 'Available', '', 'uploads/B02.jpg'),
+('B03', 'Seafood & Cheese Pizza', 19.90, 'Available', '', 'uploads/B03.jpg'),
+('B04', 'Chicken & Mushroom Pizza', 19.90, 'Available', '', 'uploads/B04.jpg'),
+('B05', 'Triple Cheese Pizza', 19.90, 'Available', '', 'uploads/B05.jpg'),
+('B06', 'Beef & Chicken Pizza', 19.90, 'Available', '', 'uploads/B06.jpg'),
+('B07', 'BBQ Chicken Pizza', 19.90, 'Available', '', 'uploads/B07.jpg');
 
 -- --------------------------------------------------------
 
@@ -212,24 +217,25 @@ CREATE TABLE `snackfood` (
   `product_name` varchar(100) NOT NULL,
   `product_price` decimal(5,2) NOT NULL,
   `product_status` varchar(20) NOT NULL,
-  `product_description` text DEFAULT NULL
+  `product_description` text DEFAULT NULL,
+  `product_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `snackfood`
 --
 
-INSERT INTO `snackfood` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`) VALUES
-('E01', 'Garlic Bread', 3.90, 'Available', NULL),
-('E02', 'Mushroom Soup', 6.90, 'Available', NULL),
-('E03', 'Mashed Potato', 4.90, 'Available', NULL),
-('E04', 'Golden Mantou', 6.90, 'Available', NULL),
-('E05', 'French Fries', 7.90, 'Available', NULL),
-('E06', 'Cheese Wedges', 8.90, 'Available', NULL),
-('E07', 'Tempura Nugget', 8.90, 'Available', NULL),
-('E08', 'Jumbo Sausage', 9.90, 'Available', NULL),
-('E09', 'Fried Abalone Mushroom', 8.90, 'Available', NULL),
-('E10', 'Chicken Popcorn', 12.90, 'Available', NULL);
+INSERT INTO `snackfood` (`product_id`, `product_name`, `product_price`, `product_status`, `product_description`, `product_image`) VALUES
+('E01', 'Garlic Bread', 3.90, 'Available', '', 'uploads/E01.jpg'),
+('E02', 'Mushroom Soup', 6.90, 'Available', '', 'uploads/E02.jpg'),
+('E03', 'Mashed Potato', 4.90, 'Available', '', 'uploads/E03.jpg'),
+('E04', 'Golden Mantou', 6.90, 'Available', '', 'uploads/E04.jpg'),
+('E05', 'French Fries', 7.90, 'Available', '', 'uploads/E05.jpg'),
+('E06', 'Cheese Wedges', 8.90, 'Available', '', 'uploads/E06.jpg'),
+('E07', 'Tempura Nugget', 8.90, 'Available', '', 'uploads/E07.jpg'),
+('E08', 'Jumbo Sausage', 9.90, 'Available', '', 'uploads/E08.jpg'),
+('E09', 'Fried Abalone Mushroom', 8.90, 'Available', '', 'uploads/E09.jpg'),
+('E10', 'Chicken Popcorn', 12.90, 'Available', '', 'uploads/E10.jpg');
 
 --
 -- Indexes for dumped tables
