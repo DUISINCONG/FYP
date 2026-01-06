9fead07573bbf9d6e7f006feaa7bd5257445c0c1
Keishav15 <1221207387@student.mmu.edu.my>
2026-01-07 01:43:51 +0800

    Update db_connect.php

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -9,4 +9,12 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
+
+if ($conn) {
+    mysqli_query($conn, "SET time_zone = '+08:00'");
+}
+
 ?>
+
