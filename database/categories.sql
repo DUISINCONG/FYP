d9869d5176e7e422b4bf015e72110a7228dd86f5
jiete41 <jerrysia3338@gmail.com>
2026-01-07 19:28:52 +0800

    Update categories.sql with Chinese comments and anchor field

--- database/categories.sql
+++ database/categories.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 07, 2026 at 07:57 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- 主机： 127.0.0.1
+-- 生成日期： 2026-01-07 12:28:04
+-- 服务器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fyp`
+-- 数据库： `fyp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- 表的结构 `categories`
 --
 
 CREATE TABLE `categories` (
@@ -32,26 +32,27 @@ CREATE TABLE `categories` (
   `category_name` varchar(100) NOT NULL,
   `category_prefix` char(1) NOT NULL,
   `table_name` varchar(100) NOT NULL,
+  `anchor` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `categories`
+-- 转存表中的数据 `categories`
 --
 
-INSERT INTO `categories` (`category_id`, `category_name`, `category_prefix`, `table_name`, `created_at`) VALUES
-(1, 'Main Food', 'A', 'main_food', '2025-12-13 12:46:10'),
-(2, 'Pizza', 'B', 'pizza', '2025-12-13 12:46:10'),
-(3, 'Coffee', 'C', 'coffee', '2025-12-13 12:46:10'),
-(4, 'Beverages', 'D', 'beverages', '2025-12-13 12:46:10'),
-(5, 'Snack Food', 'E', 'snackfood', '2025-12-13 12:46:10');
+INSERT INTO `categories` (`category_id`, `category_name`, `category_prefix`, `table_name`, `anchor`, `created_at`) VALUES
+(1, 'Main Food', 'A', 'main_food', 'mf', '2025-12-13 12:46:10'),
+(2, 'Pizza', 'B', 'pizza', 'pz', '2025-12-13 12:46:10'),
+(3, 'Coffee', 'C', 'coffee', 'cf', '2025-12-13 12:46:10'),
+(4, 'Beverages', 'D', 'beverages', 'bv', '2025-12-13 12:46:10'),
+(5, 'Snack Food', 'E', 'snackfood', 'sf', '2025-12-13 12:46:10');
 
 --
--- Indexes for dumped tables
+-- 转储表的索引
 --
 
 --
--- Indexes for table `categories`
+-- 表的索引 `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`category_id`),
@@ -59,14 +60,14 @@ ALTER TABLE `categories`
   ADD UNIQUE KEY `table_name` (`table_name`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在导出的表使用AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `categories`
+-- 使用表AUTO_INCREMENT `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
