512a36990aaffa10bb3d98cddebb7558834b2d1d
jiete41 <jerrysia3338@gmail.com>
2026-01-10 15:54:17 +0800

    Update product title and image source path

--- productDetail.php
+++ productDetail.php
@@ -27,7 +27,7 @@ $data = mysqli_fetch_assoc($result);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="productdetailcss.css"/>
-    <title>Document</title>
+    <title>Menu</title>
     <style>
         div.down {
             position: absolute;
@@ -117,7 +117,7 @@ $data = mysqli_fetch_assoc($result);
     </header>
 
     <div style="margin-left: 20%; margin-right: 20%; margin-top: 100px;">
-        <img style="height: 600px; width: 600px; float: left; margin: 20px; border-radius: 36px;" src="<?php echo $data['product_image']; ?>" alt="Food Image">
+        <img style="height: 600px; width: 600px; float: left; margin: 20px; border-radius: 36px;" src="<?php echo "backend/" . $data['product_image']; ?>" alt="Food Image">
         <h1 style="padding: 20px 0px 0px 20px; font-size: 30px; font-family: 'Poppins', 'Segoe UI', Arial, sans-serif; font-weight: 600;"><?php echo $data['product_name']; ?></h1>
         <h1 style="font-size: 26px; font-family: 'Montserrat', Arial, sans-serif; font-weight: 700; color: orange;">RM <?php echo $data['product_price']; ?></h1>
         <p style="font-size: 19px; font-family: 'Arial', 'Helvetica Neue', 'Microsoft YaHei', sans-serif; line-height: 1.6; color: #555; text-align: justify;"><?php echo $data['product_description']; ?></p>
